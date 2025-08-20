e498c6abd1507b8baebc3a5a8ad6ee88eaa18bdd
rgniteshraj <107296946+rgniteshraj@users.noreply.github.com>
2025-08-20 15:37:34 +0530

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,15 +4,14 @@ import { verifyToken } from '../middleware/authMiddleware.js';
 import {
   addReview,
   getReviews,
-  deleteReview,
-  getProductsSortedByRating
+  deleteReview
 } from '../controllers/reviewController.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, upload.array('media', 5), addReview);
 router.delete('/:reviewId', verifyToken, deleteReview);
-router.get('/products/sorted-by-rating', getProductsSortedByRating);
 router.get('/:productId', getReviews);
 export default router;
+
 
