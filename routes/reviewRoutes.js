63f5e5c6fc8771fe728c04bb0ba9b798744b3060
rgniteshraj <107296946+rgniteshraj@users.noreply.github.com>
2025-08-21 15:29:51 +0530

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.post('/', verifyToken, upload.single('media'), addReview);
+router.post('/', verifyToken, upload.array('media', 5), addReview);
 router.delete('/:reviewId', verifyToken, deleteReview);
 router.get('/:productId', getReviews);
 export default router;
