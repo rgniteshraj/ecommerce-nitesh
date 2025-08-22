c1acba25a4f2355f886a5945f57a85b9da6c17cc
rgniteshraj <107296946+rgniteshraj@users.noreply.github.com>
2025-08-22 15:14:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-//import path from 'path';
+import path from 'path';
 import reviewRoutes from './routes/reviewRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 dotenv.config();
@@ -15,4 +15,5 @@ app.use((err, req, res, next) => {
 });
 
 export default app;
+
 
