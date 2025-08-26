bd29dc8511b65c1132e5a1b28f8ba711dc3c49e3
rgniteshraj <107296946+rgniteshraj@users.noreply.github.com>
2025-08-26 15:40:10 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -4,14 +4,12 @@ const reviewSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   rating: { type: Number, required: true },
-  title: { type: String, required: true }, 
-  media: [{
-      url: { type: String, required: true },        // Cloudinary URL
-      public_id: { type: String, required: true }   // Cloudinary public_id
-    }], 
+  title: { type: String, required: true },  
   reviewDate: { type: Date, default: Date.now },
   comment: { type: String },
-  status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' }
+  status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+  verifiedBuyer: { type: Boolean, default: false }  
 });
 
 export default mongoose.model('Review', reviewSchema);
+
