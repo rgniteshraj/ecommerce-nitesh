4e1139826559b6b8a748ca2263f2eeafa8c66a0e
rgniteshraj <107296946+rgniteshraj@users.noreply.github.com>
2025-08-28 09:05:23 +0530

    Add files via upload

---
+++ routes/productRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import upload from '../middlewares/upload.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.post('/', upload.any(), createProduct);
+router.put('/:id', upload.any(), updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
---
+++ routes/reportRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { getSalesRange, getSalesFrom, getSalesTo, getTopProductsRange, getTopProductsFrom, getTopProductsTo, getInventoryAlerts } from '../controllers/reportController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+// SALES
+router.get('/sales/range', getSalesRange);
+router.get('/sales/from', getSalesFrom);
+router.get('/sales/to', getSalesTo);
+
+// TOP PRODUCTS
+router.get('/top-products/range', getTopProductsRange);
+router.get('/top-products/from', getTopProductsFrom);
+
+router.get('/top-products/to', getTopProductsTo);
+
+//Inventory Alerts
+router.get('/inventory', getInventoryAlerts);
+
+export default router;
